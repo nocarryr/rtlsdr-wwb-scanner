f34a2a52fb38095ca651a95ab5b5562d46c9b00a
nocarryr <matt@nomadic-recording.com>
2015-06-18 18:44:43 -0500

    add CSV exporter (hopefully formatted correctly for WWB

--- wwb_scanner/file_handlers/exporters.py
+++ wwb_scanner/file_handlers/exporters.py
@@ -1,1 +1,35 @@
+import os
 
+class CSVExporter(object):
+    newline_chars = '\r\n'
+    delimiter_char = ','
+    def __init__(self, **kwargs):
+        self.spectrum = kwargs.get('spectrum')
+        self.filename = kwargs.get('filename')
+    @property
+    def filename(self):
+        return getattr(self, '_filename', None)
+    @filename.setter
+    def filename(self, value):
+        if value is None:
+            return 
+        if os.path.splitext(value)[1] == '.CSV':
+            value = '.'.join([os.path.splitext(value)[0], 'csv'])
+        if value == self.filename:
+            return
+        self._filename = value
+    def build_data(self):
+        newline_chars = self.newline_chars
+        delim = self.delimiter_char
+        lines = []
+        for sample in self.spectrum.iter_samples():
+            lines.append(delim.join([
+                sample.formatted_frequency, 
+                sample.formatted_magnitude
+            ]))
+        return newline_chars.join(lines)
+    def write_file(self):
+        s = self.build_data()
+        with open(self.filename, 'w') as f:
+            f.write(s)
+        
