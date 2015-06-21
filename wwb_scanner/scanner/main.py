733494c60e05ba4f9cab0525909f9f5e0041674a
nocarryr <matt@nomadic-recording.com>
2015-06-21 13:21:18 -0500

    failed attempts at realtime redraw of plots

--- wwb_scanner/scanner/main.py
+++ wwb_scanner/scanner/main.py
@@ -1,7 +1,10 @@
+import threading
+
 from rtlsdr import RtlSdr
 
 from wwb_scanner.scanner import sample_processing
 from wwb_scanner.scan_objects import Spectrum
+from wwb_scanner.ui import plots
 
 SCANNER_DEFAULTS = dict(
     scan_range=[400., 900.], 
@@ -13,6 +16,9 @@ def mhz_to_hz(mhz):
     return mhz * 1000000.0
 def hz_to_mhz(hz):
     return hz / 1000000.0
+    
+class StopScanner(Exception):
+    pass
     
 class Scanner(object):
     '''
@@ -38,6 +44,8 @@ class Scanner(object):
     @current_freq.setter
     def current_freq(self, value):
         self._current_freq = value
+        self.on_current_freq(value)
+    def on_current_freq(self, value):
         print 'scanning %s' % (value)
     @property
     def progress(self):
@@ -48,6 +56,8 @@ class Scanner(object):
         if value == self._progress:
             return
         self._progress = value
+        self.on_progress(value)
+    def on_progress(self, value):
         print '%s%%' % (int(value * 100))
     def run_scan(self):
         freq, end_freq = self.scan_range
@@ -57,9 +67,8 @@ class Scanner(object):
         step = 0
         while freq < end_freq:
             self.current_freq = freq
-            try:
-                self.scan_freq(mhz_to_hz(freq))
-            except KeyboardInterrupt:
+            r = self.scan_freq(mhz_to_hz(freq))
+            if r is False:
                 break
             freq += step_size
             step += 1
@@ -71,3 +80,58 @@ class Scanner(object):
         for f, p in zip(freqs, powers):
             f = hz_to_mhz(f)
             spectrum.add_sample(frequency=f, magnitude=p)
+        
+class ThreadedScanner(threading.Thread, Scanner):
+    def __init__(self, **kwargs):
+        threading.Thread.__init__(self)
+        Scanner.__init__(self, **kwargs)
+        self.plot = kwargs.get('plot')
+        self.run_once = kwargs.get('run_once', True)
+        self.scan_wait_timeout = kwargs.get('scan_wait_timeout', 5.)
+        self.scanning = threading.Event()
+        self.waiting = threading.Event()
+        self.stopping = threading.Event()
+        self.stopped = threading.Event()
+        self.need_update = threading.Event()
+        self.need_update_lock = threading.Lock()
+    def on_current_freq(self, value):
+        if self.plot is not None:
+            self.plot.update_plot()
+        with self.need_update_lock:
+            self.need_update.set()
+    def run(self):
+        scanning = self.scanning
+        waiting = self.waiting
+        stopping = self.stopping
+        stopped = self.stopped
+        scan_wait_timeout = self.scan_wait_timeout
+        run_once = self.run_once
+        run_scan = self.run_scan
+        while True:
+            if stopping.is_set():
+                break
+            scanning.set()
+            run_scan()
+            scanning.clear()
+            if run_once:
+                break
+            waiting.wait(scan_wait_timeout)
+        stopped.set()
+    def scan_freq(self, freq):
+        if self.stopping.is_set():
+            return False
+        super(ThreadedScanner, self).scan_freq(freq)
+    def stop(self):
+        self.stopping.set()
+        self.waiting.set()
+        self.stopped.wait()
+        
+def scan_and_plot(**kwargs):
+    scanner = ThreadedScanner(**kwargs)
+    plot = plots.Spectrum(spectrum=scanner.spectrum)
+    scanner.plot = plot
+    scanner.start()
+    scanner.spectrum.data_updated.wait()
+    plot.build_plot()
+    scanner.stop()
+    return scanner
--- wwb_scanner/ui/plots.py
+++ wwb_scanner/ui/plots.py
@@ -4,17 +4,63 @@ import matplotlib.pyplot as plt
 class BasePlot(object):
     def __init__(self, **kwargs):
         self.spectrum = kwargs.get('spectrum')
+        
+        #self.figure.canvas.mpl_connect('idle_event', self.on_idle)
+        
+    @property
+    def x(self):
+        return getattr(self, '_x', None)
+    @x.setter
+    def x(self, value):
+        self._x = value
+    @property
+    def y(self):
+        return getattr(self, '_y', None)
+    @y.setter
+    def y(self, value):
+        self._y = value
+    @property
+    def figure(self):
+        return getattr(self, '_figure', None)
+    @figure.setter
+    def figure(self, figure):
+        self._figure = figure
+        #self.timer = figure.canvas.new_timer(interval=100)
+        #self.timer.add_callback(self.on_timer)
+    def on_timer(self):
+        print 'timer'
+        spectrum = self.spectrum
+        with spectrum.data_update_lock:
+            if spectrum.data_updated.is_set():
+                print 'update plot'
+                self.update_plot()
+                spectrum.data_updated.clear()
     def build_data(self):
         dtype = np.dtype(float)
-        self.x = np.fromiter(self.spectrum.iter_frequencies(), dtype)
-        self.y = np.fromiter((s.magnitude for s in self.spectrum.iter_samples()), dtype)
+        if not len(self.spectrum.samples):
+            x = self.x = np.array(0.)
+            y = self.y = np.array(0.)
+        else:
+            x = self.x = np.fromiter(self.spectrum.iter_frequencies(), dtype)
+            y = self.y = np.fromiter((s.magnitude for s in self.spectrum.iter_samples()), dtype)
+            if not hasattr(self, 'plot'):
+                self.spectrum.data_updated.clear()
+        return x, y
+    def update_plot(self):
+        if not hasattr(self, 'plot'):
+            return
+        x, y = self.build_data()
+        self.plot.set_xdata(x)
+        self.plot.set_ydata(y)
+        #self.figure.canvas.draw_event(self.figure.canvas)
+        self.figure.canvas.draw_idle()
     def build_plot(self):
         pass
     
 class Spectrum(BasePlot):
     def build_plot(self):
-        plt.figure()
-        plt.plot(self.x, self.y)
+        self.figure = plt.figure()
+        self.plot = plt.plot(self.build_data())[0]
         plt.xlabel('frequency (MHz)')
         plt.ylabel('dBm')
         plt.show()
