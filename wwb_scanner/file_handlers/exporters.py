6e70a85bc411bbed660cf63f5513663943acc9b6
nocarryr <matt@nomadic-recording.com>
2015-06-20 19:26:21 -0500

    add base exporter class

--- wwb_scanner/file_handlers/exporters.py
+++ wwb_scanner/file_handlers/exporters.py
@@ -1,8 +1,6 @@
 import os
 
-class CSVExporter(object):
-    newline_chars = '\r\n'
-    delimiter_char = ','
+class BaseExporter(object):
     def __init__(self, **kwargs):
         self.spectrum = kwargs.get('spectrum')
         self.filename = kwargs.get('filename')
@@ -13,11 +11,25 @@ class CSVExporter(object):
     def filename(self, value):
         if value is None:
             return 
-        if os.path.splitext(value)[1] == '.CSV':
-            value = '.'.join([os.path.splitext(value)[0], 'csv'])
+        self.set_filename(value)
+    def set_filename(self, value):
         if value == self.filename:
             return
         self._filename = value
+    def build_data(self):
+        raise NotImplementedError('Method must be implemented by subclasses')
+    def write_file(self):
+        s = self.build_data()
+        with open(self.filename, 'w') as f:
+            f.write(s)
+        
+class CSVExporter(BaseExporter):
+    newline_chars = '\r\n'
+    delimiter_char = ','
+    def set_filename(self, value):
+        if os.path.splitext(value)[1] == '.CSV':
+            value = '.'.join([os.path.splitext(value)[0], 'csv'])
+        super(CSVExporter, self).set_filename(value)
     def build_data(self):
         newline_chars = self.newline_chars
         delim = self.delimiter_char
@@ -28,8 +40,4 @@ class CSVExporter(object):
                 sample.formatted_magnitude
             ]))
         return newline_chars.join(lines)
-    def write_file(self):
-        s = self.build_data()
-        with open(self.filename, 'w') as f:
-            f.write(s)
         
