31ab208a7633f1d2221ff9c33b51c14713c514fd
nocarryr <matt@nomadic-recording.com>
2015-07-01 20:21:34 -0500

    play with parameters

--- wwb_scanner/scanner/rtlpower_scan.py
+++ wwb_scanner/scanner/rtlpower_scan.py
@@ -4,12 +4,16 @@ from wwb_scanner.scanner.main import ScannerBase, hz_to_mhz
 
 class RtlPowerScanner(ScannerBase):
     def run_scan(self):
-        cmd_str = 'rtl_power -f %fM:%fM:%fM -g %s -1 -'
+        fir_size = 4
+        crop_size = 50
+        cmd_str = 'rtl_power -f %fM:%fM:%fM -c %s%% -g %s -F %s -1 -'
         cmd_str = cmd_str % (
             self.scan_range[0], 
             self.scan_range[1], 
-            self.step_size, 
+            self.step_size / 2., 
+            crop_size, 
             self.gain, 
+            fir_size, 
         )
         self.result = subprocess.check_output(cmd_str, shell=True)
         spectrum = self.spectrum
