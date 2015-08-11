401dd586321c72e21af863b7fd575dbb6caf3fae
nocarryr <matt@nomadic-recording.com>
2015-08-11 15:55:36 -0500

    TinyDB now sucessfully writes to files

--- wwb_scanner/utils/dbstore.py
+++ wwb_scanner/utils/dbstore.py
@@ -2,8 +2,6 @@ import os
 import datetime
 
 from tinydb import TinyDB, where
-from tinydb.storages import JSONStorage
-from tinydb.middlewares import CachingMiddleware
 
 APP_PATH = os.path.expanduser('~/wwb_scanner_data')
 
@@ -14,37 +12,35 @@ class DBStore(object):
     def __init__(self):
         if not os.path.exists(os.path.dirname(DB_PATH)):
             os.makedirs(os.path.dirname(DB_PATH))
-        self.db = TinyDB(DB_PATH, storage=CachingMiddleware(JSONStorage))
+        self.db = TinyDB(DB_PATH)
     def add_scan_config(self, config, force_insert=False):
         if config.get('datetime') is None:
             config.datetime = datetime.datetime.utcnow()
         table = self.db.table('scan_configs')
-        if True:#with table:
-            if config.get('eid') is not None:
-                dbconfig = table.get(eid=config.eid)
-            else:
-                dbconfig = table.get(where('datetime') == config.datetime)
-            if dbconfig is not None:
-                if force_insert:
-                    eids = [dbconfig.eid]
-                    table.update(config._serialize(), eids=eids)
-                eid = dbconfig.eid
-            else:
-                eid = table.insert(config._serialize())
+        if config.get('eid') is not None:
+            dbconfig = table.get(eid=config.eid)
+        else:
+            dbconfig = table.get(where('datetime') == config.datetime)
+        if dbconfig is not None:
+            if force_insert:
+                eids = [dbconfig.eid]
+                table.update(config._serialize(), eids=eids)
+            eid = dbconfig.eid
+        else:
+            eid = table.insert(config._serialize())
         config.eid = eid
     def get_scan_config(self, **kwargs):
         table = self.db.table('scan_configs')
-        if True:#with table:
-            if kwargs.get('datetime'):
-                dbconfig = table.get(where('datetime')==kwargs.get('datetime'))
-            elif kwargs.get('eid'):
-                dbconfig = table.get(eid=kwargs.get('eid'))
-            elif kwargs.get('name'):
-                dbconfig = table.get(where('name')==kwargs.get('name'))
-            elif table._last_id > 0:
-                dbconfig = table.get(eid=table._last_id)
-            else:
-                dbconfig = None
+        if kwargs.get('datetime'):
+            dbconfig = table.get(where('datetime')==kwargs.get('datetime'))
+        elif kwargs.get('eid'):
+            dbconfig = table.get(eid=kwargs.get('eid'))
+        elif kwargs.get('name'):
+            dbconfig = table.get(where('name')==kwargs.get('name'))
+        elif table._last_id > 0:
+            dbconfig = table.get(eid=table._last_id)
+        else:
+            dbconfig = None
         return dbconfig
     def add_scan(self, spectrum, scan_config):
         if scan_config.get('eid') is None:
@@ -52,12 +48,10 @@ class DBStore(object):
         data = spectrum._serialize()
         data['scan_config_eid'] = scan_config.eid
         table = self.db.table('scans_performed')
-        if True:#with table:
-            table.insert(data)
+        table.insert(data)
     def get_all_scans(self):
         table = self.db.table('scans_performed')
-        if True:#with table:
-            scans = table.all()
+        scans = table.all()
         return scans
     
 db_store = DBStore()
