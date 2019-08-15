nocarryr/rtlsdr-wwb-scanner

commit 9e683f462732b7f250ca1461a6e8d536017a49a4

Centralize decibel calculations

wwb_scanner/utils/dbmath.py
@@ -1,0 +1,21 @@
+import numpy as np
+
+REF_DB = 1.#1e-4
+
+def amplitude_to_dB(a, ref=REF_DB):
+    return 20 * np.log10(a / ref)
+
+def dB_to_amplitude(dB, ref=REF_DB):
+    return 10 ** (dB/20.) * ref
+
+def power_to_dB(p, ref=REF_DB):
+    return 10 * np.log10(p / ref)
+
+def dB_to_power(dB, ref=REF_DB):
+    return 10 ** (dB/10.) * ref
+
+def to_dB(p, ref=REF_DB):
+    return power_to_db(p, ref)
+
+def from_dB(dB, ref=REF_DB):
+    return dB_to_power(dB, ref)

wwb_scanner/scan_objects/sample.py
@@ -3,6 +3,7 @@ import numbers
 import numpy as np
 
 from wwb_scanner.core import JSONMixin
+from wwb_scanner.utils import dbmath
 
 class Sample(JSONMixin):
     def __init__(self, **kwargs):
@@ -93,7 +94,7 @@ class Sample(JSONMixin):
                 return
         if not isinstance(value, numbers.Number):
             return
-        m = 10 ** (value / 10.)
+        m = dbmath.from_dB(value)
         ix = self.spectrum_index
         self.spectrum.sample_data['dbFS'][ix] = value
         self.spectrum.sample_data['magnitude'][ix] = m

wwb_scanner/scan_objects/samplearray.py
@@ -3,6 +3,7 @@ from scipy.interpolate import CubicSpline
 import jsonfactory
 
 from wwb_scanner.core import JSONMixin
+from wwb_scanner.utils import dbmath
 
 class SampleArray(JSONMixin):
     dtype = np.dtype([
@@ -50,9 +51,9 @@ class SampleArray(JSONMixin):
         if iq is not None and mag is None:
             mag = data['magnitude'] = np.abs(iq)
         if dbFS is not None and mag is None:
-            mag = data['magnitude'] = 10 ** (dbFS / 10)
+            mag = data['magnitude'] = dbmath.from_dB(dbFS)
         if mag is not None and dbFS is None:
-            data['dbFS'] = 10 * np.log10(mag)
+            data['dbFS'] = dbmath.to_dB(mag)
 
         self.append(data)
     def __getattr__(self, attr):
@@ -117,7 +118,7 @@ class SampleArray(JSONMixin):
             raise Exception('Smooth result size {} != data size {}'.format(m.size, x.size))
 
         self.data['magnitude'] = m
-        self.data['dbFS'] = 10 * np.log10(m)
+        self.data['dbFS'] = dbmath.to_dB(m)
     def interpolate(self, spacing=0.025):
         fmin = np.ceil(self.frequency.min())
         fmax = np.floor(self.frequency.max())
@@ -133,7 +134,7 @@ class SampleArray(JSONMixin):
         data = np.zeros(xs.size, dtype=self.dtype)
         data['frequency'] = xs
         data['magnitude'] = ys
-        data['dbFS'] = 10 * np.log10(ys)
+        data['dbFS'] = dbmath.to_dB(ys)
         self.data = data
 
     def _serialize(self):

wwb_scanner/scan_objects/spectrum.py
@@ -6,6 +6,7 @@ import numpy as np
 from scipy import signal
 
 from wwb_scanner.core import JSONMixin
+from wwb_scanner.utils import dbmath
 from wwb_scanner.utils.dbstore import db_store
 from wwb_scanner.utils.color import Color
 from wwb_scanner.scan_objects import SampleArray, Sample, TimeBasedSample
@@ -189,7 +190,7 @@ class Spectrum(JSONMixin):
             y /= y.max()
             y *= out_scale
             y += min_dB
-            self.sample_data['magnitude'] = 10 ** (y / 10)
+            self.sample_data['magnitude'] = dbmath.from_dB(y)
             self.sample_data['dbFS'] = y
         self.set_data_updated()
     def add_sample(self, **kwargs):

commit 5d96b6b1dca0a5f9427a088594ee42e2f8c55c4c

Correct typo introduced in 9e683f46

wwb_scanner/utils/dbmath.py
@@ -15,7 +15,7 @@ def dB_to_power(dB, ref=REF_DB):
     return 10 ** (dB/10.) * ref
 
 def to_dB(p, ref=REF_DB):
-    return power_to_db(p, ref)
+    return power_to_dB(p, ref)
 
 def from_dB(dB, ref=REF_DB):
     return dB_to_power(dB, ref)