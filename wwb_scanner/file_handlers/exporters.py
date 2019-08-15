712caa633196b1b26d4c64e08a2df5f8b663fd1c
nocarryr <matt@nomadic-recording.com>
2019-08-15 09:10:46 -0500

    Filter out NaN and -inf values before exporting

--- wwb_scanner/file_handlers/exporters.py
+++ wwb_scanner/file_handlers/exporters.py
@@ -163,6 +163,8 @@ class WWBExporter(BaseWWBExporter):
         data_set = ET.SubElement(data_sets, 'data_set', self.attribs['data_set'])
         freqs = self.spectrum.sample_data['frequency'] * 1000
         dB = np.around(self.spectrum.sample_data['dbFS'], decimals=1)
+        nanix = np.flatnonzero(np.isnan(dB) | np.isinf(dB))
+        dB[nanix] = -140.
         for freq, val in zip(freqs, dB):
             f = ET.SubElement(freq_set, 'f')
             f.text = str(int(freq))
