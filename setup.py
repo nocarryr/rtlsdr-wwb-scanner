197a09a82399924521030af539fe445b6cbfac65
nocarryr <matt@nomadic-recording.com>
2019-08-31 12:05:02 -0500

    Require numpy>=1.13

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
     install_requires=[
-        'numpy',
+        'numpy>=1.13',
         'scipy',
         'pyrtlsdr>=0.2.6',
         'tinydb',
