93cbc014c491cfea5e8f4f33ba6b92ddfc4419c8
nocarryr <matt@nomadic-recording.com>
2019-09-12 10:40:16 -0500

    Move setuptools data into setup.cfg

--- setup.py
+++ setup.py
@@ -1,62 +1,9 @@
-import sys
-
-from setuptools import setup, find_packages
-
-def convert_readme():
-    try:
-        import pypandoc
-    except ImportError:
-        return read_rst()
-    rst = pypandoc.convert_file('README.md', 'rst')
-    with open('README.rst', 'w') as f:
-        f.write(rst)
-    return rst
-
-def read_rst():
-    try:
-        with open('README.rst', 'r') as f:
-            rst = f.read()
-    except IOError:
-        rst = None
-    return rst
-
-def get_long_description():
-    if {'sdist', 'bdist_wheel'} & set(sys.argv):
-        long_description = convert_readme()
-    else:
-        long_description = read_rst()
-    return long_description
+from setuptools import setup
 
 setup(
-    name = "rtlsdr-wwb-scanner",
-    version = "0.0.1",
-    author = "Matthew Reid",
-    author_email = "matt@nomadic-recording.com",
-    url='https://github.com/nocarryr/rtlsdr-wwb-scanner',
-    description = "SDR Scanner",
-    license='GPLv2',
-    packages=find_packages(exclude=['tests*']),
-    include_package_data=True,
-    install_requires=[
-        'numpy>=1.13',
-        'scipy',
-        'pyrtlsdr>=0.2.6',
-        'tinydb',
-        'json-object-factory',
-        'PySide2>=5.13.1',
-    ],
-    setup_requires=['pypandoc'],
-    long_description=get_long_description(),
     entry_points={
         'console_scripts':[
             'wwb_scanner-ui = wwb_scanner.ui.pyside.main:run',
         ],
     },
-    platforms=['any'],
-    classifiers = [
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-    ],
 )
